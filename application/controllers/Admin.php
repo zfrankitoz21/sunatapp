d839756a2272682e9631792eed1846fff256f514
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-03-26 15:05:20 -0500

    fix

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,9 +12,21 @@ class Admin extends CI_Controller {
 		$this->load->view('admin');
 	}
 
-	public function categorias() {
+	public function categorias($id = false) {
         $this->load->model('mcategorias');
-        $data['data'] = $this->mcategorias->categorias_entrys();
-		$this->load->view('admin/categorias', $data);
+		$data['data'] = $this->mcategorias->categorias_entrys($id);
+        if ( $id )
+        	$this->load->view('admin/categoriasedit', $data);
+        else
+			$this->load->view('admin/categorias', $data);
+	}
+	public function categoriasEdit($id) {
+		$this->load->model('mcategorias');
+		$data = array (
+			'id' => $id,
+			'categoria' => $this->input->post('categoria'),
+			'color' => $this->input->post('color') 
+		);
+		$data['data'] = $this->mcategorias->categorias_update($data);
 	}
 }
