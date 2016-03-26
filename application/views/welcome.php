7685ae008f7af8da2e83fe98f884aec0e5375b71
Diego Abanto <dabanto21@gmail.com>
2016-03-26 15:14:02 -0500

    prueba

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -16,6 +16,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<li><a href="#">Administración de Destacadas</a></li>
 			<li><a href="#">Administración de Items</a></li>
 			<li><a href="#">Administración de Empresas</a></li>
+                        <li><a href="#">Prueba</a></li>
 		</ul>
 	</div>
 
