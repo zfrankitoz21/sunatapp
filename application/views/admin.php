6f1b07f3ea7122e6c293125ab40b8b8b33607974
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-03-26 13:25:57 -0500

    fix

---
+++ application/views/admin.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>SUNAT INTRANET</title>
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css">
+</head>
+<body>
+
+	<div id="container">
+		<h1>Bienvenidos al administrador de contenido SUNAT</h1>
+		<ul>
+			<li><a href="#">Administración de Categorías</a></li>
+			<li><a href="#">Administración de Destacadas</a></li>
+			<li><a href="#">Administración de Items</a></li>
+			<li><a href="#">Administración de Empresas</a></li>
+		</ul>
+	</div>
+
+</body>
+</html>
