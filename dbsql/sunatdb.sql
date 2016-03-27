ed8ff5ef98a7ea2f57d049cc046f7beab957e2c2
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-03-27 13:33:01 -0500

    css

--- dbsql/sunatdb.sql
+++ dbsql/sunatdb.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.4.12
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 26-03-2016 a las 22:09:21
--- Versión del servidor: 5.6.25
--- Versión de PHP: 5.6.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `sunatdb`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categorias`
---
 
 CREATE TABLE IF NOT EXISTS `categorias` (
   `id` int(11) NOT NULL,
@@ -33,49 +14,48 @@ CREATE TABLE IF NOT EXISTS `categorias` (
   `imagen` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `categorias`
---
-
 INSERT INTO `categorias` (`id`, `categoria`, `color`, `imagen`) VALUES
-(1, 'Entretenimiento', '#0063a7', 0),
-(2, 'Belleza y Salud', '', 0),
-(3, 'Gastronomia', '', 0),
-(4, 'En Familia', '', 0),
+(1, 'Entretenimiento3', '#0063a7', 1),
+(2, 'Belleza y Salud2', '#FFFFF', 2),
+(3, 'Gastronomia3', '#525262', 8),
+(4, 'En Familia', '#0063a7', 16),
 (5, 'Educación', '', 0),
 (6, 'Detalles y Obsequios', '', 0),
 (7, 'Tiempo libre / Viajes', '', 0);
 
---
--- Índices para tablas volcadas
---
+CREATE TABLE IF NOT EXISTS `empresas` (
+  `id` int(11) NOT NULL,
+  `empresa` varchar(30) NOT NULL,
+  `imagen` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
---
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `empresas` (`id`, `empresa`, `imagen`) VALUES
+(1, 'Multident', 14),
+(3, 'Starbucks', 15);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `fecha` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
---
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
---
--- Estructura de tabla para la tabla `tbl_promociones`
---
+INSERT INTO `files` (`id`, `nombre`, `fecha`) VALUES
+(1, 'salud2.jpg', 1459051372),
+(2, 'regalo.jpg', 1459060561),
+(3, 'impsobile.jpg', 1459061973),
+(5, 'emprendedor.png', 1459062111),
+(8, '12244001_10206396543085453_893107207_n.jpg', 1459062523),
+(9, 'familia2.jpg', 1459067441),
+(10, 'familia3.jpg', 1459067561),
+(11, 'min_icon.jpg', 1459069989),
+(12, 'min_icon1.jpg', 1459070004),
+(13, 'starbucks-128.png', 1459070105),
+(14, 'min_icon.jpg', 1459070440),
+(15, 'starbucks-128.png', 1459070444),
+(16, 'starbucks-128.png', 1459101637);
 
 CREATE TABLE IF NOT EXISTS `tbl_promociones` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `titulo` varchar(150) NOT NULL,
   `empresa` varchar(150) NOT NULL,
   `desc_empresa` varchar(256) NOT NULL,
@@ -83,13 +63,8 @@ CREATE TABLE IF NOT EXISTS `tbl_promociones` (
   `telefono` int(11) NOT NULL,
   `desc_descuento` varchar(256) NOT NULL,
   `desc_restriccion` varchar(256) NOT NULL,
-  `imagen` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
-
---
--- Volcado de datos para la tabla `tbl_promociones`
---
+  `imagen` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 INSERT INTO `tbl_promociones` (`id`, `titulo`, `empresa`, `desc_empresa`, `direccion`, `telefono`, `desc_descuento`, `desc_restriccion`, `imagen`) VALUES
 (1, 'Título 1', 'Empresa 1', 'Desc Empresa 1', 'Direccion 1', 1234567, 'Desc Descuento 1', 'Desc Restriccion 1', 9999998),
@@ -98,3 +73,25 @@ INSERT INTO `tbl_promociones` (`id`, `titulo`, `empresa`, `desc_empresa`, `direc
 (4, 'Titulo 4', 'Empresa 4 ', 'Desc Empresa 4', 'Direccion 4', 1234567, 'Desc Descuento 4', 'Desc Restriccion 4', 4),
 (5, 'Título 5', 'Empresa 5', 'Desc Empresa 5', 'Direccion 5', 278788, 'Desc Descuento 5', 'Desc Restriccion 5', 5);
 
+
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `empresas`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `tbl_promociones`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+ALTER TABLE `empresas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+ALTER TABLE `tbl_promociones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
