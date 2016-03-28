a72584be1dc48d3a387f1ae2ac80554b8b241a55
zfrankitoz21 <fk.franko.soto@gmail.com>
2016-03-27 20:28:50 -0500

    css

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	public function index() {
-		$this->load->view('promocion_view'); //index
+		$this->load->view('index'); //index
 	}
 
 	public function entretenimiento() {
